7446c8a662e86d70cf1988f3dc8be2505c51ba7f
Wenxuan Zhao <viz@linux.com>
2024-09-14 04:00:03 +0800

    Fix key display in markup texts on Windows

--- src/global.rs
+++ src/global.rs
@@ -1,6 +1,6 @@
 // TODO: move to df::globals
-use crate::config::CONFIG;
 
+use crate::config::CONFIG;
 use crate::utils;
 
 #[cfg(target_os = "linux")]
@@ -49,21 +49,16 @@ pub static GPS: usize = {
   }
 };
 
-#[allow(non_upper_case_globals)]
 #[cfg(target_os = "linux")]
-#[static_init::dynamic]
-pub static get_key_display: fn(usize, usize, i32) = unsafe {
-  match CONFIG.symbol.is_some() {
-    true => {
-      let symbol = CONFIG.symbol.as_ref().unwrap().get_key_display.as_ref().unwrap();
-      utils::symbol_handle::<fn(usize, usize, i32)>(&symbol[0], &symbol[1])
-    }
-    false => move |_, _, _| {},
-  }
-};
+pub fn get_key_display(str_ptr: usize, enabler: usize, binding: i32) {
+  let symbol = CONFIG.symbol.as_ref().unwrap().get_key_display.as_ref().unwrap();
+  let get_key_display_impl = unsafe { utils::symbol_handle::<fn(usize, usize, i32)>(&symbol[0], &symbol[1]) };
+  get_key_display_impl(str_ptr, enabler, binding);
+}
 
-#[allow(non_upper_case_globals)]
 #[cfg(target_os = "windows")]
 pub fn get_key_display(str_ptr: usize, enabler: usize, binding: i32) {
-  log::warn!("FIXME: get_key_display(0x{str_ptr:x}, 0x{enabler:x}, {binding}) doesn't work");
+  let addr = utils::address(CONFIG.offset.as_ref().unwrap().get_key_display.unwrap());
+  let get_key_display_impl: fn(usize, usize, i32) = unsafe { std::mem::transmute(addr) };
+  get_key_display_impl(enabler, str_ptr, binding);
 }
