15aee54e60a113b04aae625bf2ed0224380cdd62
Wenxuan Zhao <viz@linux.com>
2024-09-22 16:16:42 +0800

    Add data for plants growths

--- src/translator/data/plants.rs
+++ src/translator/data/plants.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{translator::data::rules, utils};
 
 use super::dictionary;
@@ -24,6 +26,14 @@ struct PlantBase {
   name_translation: String,
 }
 
+#[derive(Debug, serde::Deserialize)]
+struct PlantGrowthBase {
+  key: String,
+
+  nsg: String,
+  npl: String,
+}
+
 #[derive(Debug, Default)]
 pub struct Plants {
   pub name_singulars: dictionary::Dictionary,
@@ -36,6 +46,8 @@ pub struct Plants {
   pub light_branch_names: dictionary::Dictionary,
   pub twig_names: dictionary::Dictionary,
   pub cap_names: dictionary::Dictionary,
+  pub growth_name_singulars: dictionary::Dictionary,
+  pub growth_name_plurals: dictionary::Dictionary,
 
   pub nouns: dictionary::Dictionary,
   pub adjectives: dictionary::Dictionary,
@@ -44,14 +56,15 @@ pub struct Plants {
 impl Plants {
   fn new() -> Self {
     let mut plants = Plants::default();
-    let mut rule_set: rules::RuleSet = rules::RuleSet::default();
+    let mut plants_rule_set = rules::RuleSet::default();
+    let mut base_names = HashMap::<String, (String, String)>::default();
     utils::load_csv(utils::translations_path("plants-rules.csv"), |rule: rules::Rule| {
-      rule_set.insert_rule(rule);
+      plants_rule_set.insert_rule(rule);
     });
     utils::load_csv(
       utils::translations_path("plants-special.csv"),
       |special: rules::Special| {
-        rule_set.insert_special(special);
+        plants_rule_set.insert_special(special);
       },
     );
     utils::load_csv(
@@ -71,6 +84,8 @@ impl Plants {
          name,
          name_translation,
        }| {
+        base_names.insert(key.to_owned(), (name.to_owned(), name_translation.to_owned()));
+
         let mut context = rules::RuleContext::new();
         context.insert("NSG", (name, name_translation));
         context.insert("NPL", (npl, String::new()));
@@ -84,7 +99,7 @@ impl Plants {
         context.insert("TGN", (tgn, String::new()));
         context.insert("CPN", (cpn, String::new()));
 
-        rule_set.process(&key, &mut context);
+        plants_rule_set.process(&key, &mut context);
         for (field, (word, translated)) in context {
           if field != "ADJ" {
             plants.nouns.insert(word.to_owned(), translated.to_owned());
@@ -102,6 +117,48 @@ impl Plants {
             "LBN" => &mut plants.light_branch_names,
             "TGN" => &mut plants.twig_names,
             "CPN" => &mut plants.cap_names,
+            field => panic!("unhandled field {field:?}"),
+          };
+
+          dict.insert(word, translated);
+        }
+      },
+    );
+    let mut plants_growth_rule_set = rules::RuleSet::default();
+    utils::load_csv(
+      utils::translations_path("plants-growth-rules.csv"),
+      |rule: rules::Rule| {
+        plants_growth_rule_set.insert_rule(rule);
+      },
+    );
+    utils::load_csv(
+      utils::translations_path("plants-growth-special.csv"),
+      |special: rules::Special| {
+        plants_growth_rule_set.insert_special(special);
+      },
+    );
+    utils::load_csv(
+      utils::translations_path("plants-growth-base.csv"),
+      |PlantGrowthBase { key, nsg, npl }| {
+        let mut key_parts: Vec<&str> = key.split(':').collect();
+        key_parts.pop();
+        let base_name = base_names.get(&key_parts.join(":")).unwrap().to_owned();
+
+        let mut context = rules::RuleContext::new();
+        context.insert("NBS", base_name);
+        context.insert("NSG", (nsg, String::new()));
+        context.insert("NPL", (npl, String::new()));
+
+        plants_growth_rule_set.process(&key, &mut context);
+        for (field, (word, translated)) in context {
+          if field != "NBS" {
+            plants.nouns.insert(word.to_owned(), translated.to_owned());
+          }
+
+          let dict = match field {
+            "NBS" => continue,
+            "NSG" => &mut plants.growth_name_singulars,
+            "NPL" => &mut plants.growth_name_plurals,
             field => panic!("unhandled field {field:?}"),
           };
 
